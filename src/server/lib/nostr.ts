0901cc3e97dd3961cb6c5349d2281d4cbe9f83fc
Serkan <newkipsoftware@gmail.com>
2023-12-23 15:48:07 +0300

    Logs added.

--- src/server/lib/nostr.ts
+++ src/server/lib/nostr.ts
@@ -67,6 +67,7 @@ export default class NostrCheck extends Nostr {
                 const _filter = { kinds: [1], since, authors: [ usr.publicKey ] };
                 console.log(`User checking... ${usr.publicKey}`);
                 const _events = await this.filter(_filter).collect();
+                console.log('Events ...', _events);
                 for (const _evnt of _events) {
                     const text = `Please approve my NIP-05 request on https://nip05.nostprotocol.net @${Deno.env.get('NPUBLIC_KEY')}`;
                     console.log('Content checking...', _evnt.content);
